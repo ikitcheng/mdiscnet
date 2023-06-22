69a45c9f05a9be5b7a39557a06fc6c1c847df9ce
Matthew Cheng <ikc19@hypatia-login.local>
2023-06-22 06:42:49 +0100

    [UPDATE] Add save and load pickle results functions

--- mdiscnet/util.py
+++ mdiscnet/util.py
@@ -1,5 +1,5 @@
 import torch
-
+import pickle 
 def save_model(model, model_path):
     """ Save the model weights. """
     torch.save(model.state_dict(), model_path)
@@ -11,3 +11,14 @@ def load_model(model, model_path):
     model.to(model.device)
     model.eval()
     return model
+
+def save_result(result, filename):
+    """ Save pickle data. """
+    with open(filename, "wb") as f:
+        pickle.dump(result, f)
+
+def load_result(filename):
+    """ Load pickle data """
+    with open(filename, "rb") as f:
+        result = pickle.load(f)
+    return result
