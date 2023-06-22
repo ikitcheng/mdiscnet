398c4b4bc0f1784052a26d3153ebaf4b4d584163
Matthew Cheng <ikc19@hypatia-login.local>
2023-06-22 06:49:16 +0100

    [UPDATE] Update plot latent vectors specific to mdisc models

--- mdiscnet/plots.py
+++ mdiscnet/plots.py
@@ -3,20 +3,40 @@ import numpy as np
 import torch
 from PIL import Image
 
-def plot_latent(model, data, num_batches=100):
+def plot_latent(model, data):
+    N = len(data)
+    kh_arr = np.zeros(N)
+    rmp_arr = np.zeros(N)
+    z_arr = np.zeros((N,2))
+    
+    fig, ax = plt.subplots(1,3,sharex=False, sharey=False, figsize=(10,5))
+    
     for i, (x, y) in enumerate(data):
         z,*_ = model.encode(x.to(model.device))
         z = z.to('cpu').detach().numpy()
-        plt.scatter(z[:, 0], z[:, 1], c=y, cmap='tab10')
-        if i > num_batches:
-            plt.colorbar()
-            break
+        z_arr[i] = z
+        
+        # split label into rmp and kh colorbars
+        label = y[0].split("_")
+        kh = float(label[0][2:])
+        rmp = float(label[1][3:])
+        kh_arr[i] = kh
+        rmp_arr[i] = rmp
+        
+    s1 = ax[0].scatter(z_arr[:, 0], z_arr[:, 1], c=kh_arr, cmap='jet')
+    s2 = ax[1].scatter(z_arr[:, 0], z_arr[:, 1], c=rmp_arr, cmap='jet')
+    s3 = ax[2].scatter(rmp_arr, kh_arr)
+    ax[2].set_xlabel('rmp')
+    ax[2].set_ylabel('Kh')
+    plt.colorbar(s1, ax=ax[0], label='kh')
+    plt.colorbar(s2, ax=ax[1], label='rmp')
+    plt.tight_layout()
     
 def reconstruct(model, x, width):
     z, *_ = model.encode(x)
     x_hat = model.decode(z)
     x_hat = x_hat.reshape(width, width).to('cpu').detach().numpy()
-    plt.imshow(x_hat)
+    return x_hat
     
 def decode_and_plot(z):
     x_hat = model.decode(z)
@@ -33,7 +53,7 @@ def plot_reconstructed(model, width, z0=(-5, 5), z1=(-5, 5), n_img=12):
             x_hat = model.decode(z)
             x_hat = x_hat.reshape(width, width).to('cpu').detach().numpy()
             img[(n_img-1-i)*width:(n_img-1-i+1)*width, j*width:(j+1)*width] = x_hat
-    plt.imshow(img, extent=[*z0, *z1])
+    plt.imshow(img, extent=[*z0, *z1], cmap='jet')
 
     
 def interpolate(model, x_1, x_2, width, n_img=12):
@@ -48,7 +68,7 @@ def interpolate(model, x_1, x_2, width, n_img=12):
     img = np.zeros((width, n_img*width))
     for i, x_hat in enumerate(interpolate_list):
         img[:, i*width:(i+1)*width] = x_hat.reshape(width, width)
-    plt.imshow(img)
+    plt.imshow(img, cmap='jet')
     plt.xticks([])
     plt.yticks([])
     
