f0e25dcfb28238c01eca974c5f296a80fa278d53
Matthew Cheng <matthewkit@gmail.com>
2023-08-17 11:11:46 +0100

    [UPDATE] Include wandb tracking of training metrics

--- mdiscnet/train.py
+++ mdiscnet/train.py
@@ -2,13 +2,21 @@ import numpy as np
 import torch
 from tqdm import tqdm
 from lossfn import loss_var
+import wandb
 # Reference: https://avandekleut.github.io/vae/
 
-def train_model(model, data, lr=1e-3, epochs=20):
+def train_model(model, data, config):
+    
     model = model.to(model.device)
-    opt = torch.optim.Adam(params=model.parameters(), lr=lr)
-    hist = {'loss':np.zeros(epochs)}
-    for epoch in tqdm(range(epochs)):
+    
+    # Start wandb run for logging
+    wandb.init (project='mdiscnet', job_type='train', config=config)
+
+    opt = torch.optim.Adam(params=model.parameters(), lr=config.lr)
+    
+    hist = {'loss':np.zeros(config.epochs)}
+
+    for epoch in tqdm(range(config.epochs)):
         for x, y in data:
             x = x.to(model.device) # GPU
             opt.zero_grad()
@@ -18,7 +26,19 @@ def train_model(model, data, lr=1e-3, epochs=20):
             opt.step()
             
         hist['loss'][epoch] = loss.item()
+        
+        metrics = {
+                "train/train_loss": hist['loss'][epoch],
+                "train/epoch": epoch + 1,
+            }
+        
+        # Log metrics over time to visualize performance
+        wandb.log(metrics)
+
         # Update the progress bar with relevant information
         if epoch%100 == 0:
             tqdm.write(f"Epoch: {epoch+1}/{epochs}, Loss: {loss.item():.4f}")
+            
+    wandb.finish()
+    
     return model, hist
